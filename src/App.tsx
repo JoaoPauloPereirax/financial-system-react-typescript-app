12c8f2070bc2a151c0a64f1c8b0c9538f478119f
João Paulo Pereira <joaopaulop.dev@gmail.com>
2023-07-19 16:44:15 -0300

    Add data filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import {useState} from 'react';
 import * as C from './App.styles';
 
+import { Category } from './types/Category';
+import { Item } from './types/Item';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import {getCurrentMonth} from './helpers/dataFilter';
+
+
 function App() {
+  const [list,setList]=useState(items);
+  /*
+  Neste caso não precisamos tipar o state que ficaria assim >>
+    const [list,setList]=useState<Item[]>(items);
+*/
+const [currentMonth, setCurrentMonth] = useState('');
   return (
     <C.Container>
       <C.Header>

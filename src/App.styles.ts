29d8e33ea9c0566a8f230720920350b5f348c1de
João Paulo Pereira <joaopaulop.dev@gmail.com>
2023-07-19 15:53:44 -0300

    Add Global Styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -7,12 +7,16 @@ export const Container = styled.div`
 
 
 export const Header = styled.div`
-  
+  background-color: darkblue;
+  height: 20vh;
+  text-align: center;
 `;
 
 
 export const HeaderText = styled.h1`
-
+     margin: 0;
+     padding: 0;
+     color: ;
 `;
 
 export const Body = styled.div`
